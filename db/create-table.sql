2df791896cbaf5ef129e6c278300e8a9f5d05528
masafumi330 <mihusamawasami@gmail.com>
2025-12-30 23:43:16 +0900

    create table

---
+++ db/create-table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE todos (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    description TEXT
+);
